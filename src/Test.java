ff8582a53542b042c60c520baad23f3c10c35a87
Ashish Kumar Ashok <ashish.livein@gmail.com>
2012-12-15 23:55:38 -0500

    int i

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args){
+		int i =0;
 		Scanner input=new Scanner(System.in);
 		Triangle triangle = new Triangle();
 		
